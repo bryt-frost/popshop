cddb390d7d43b9c374f8118e0c5a5196d25e5cad
brytfrost <brytfrost@gmail.com>
2024-01-31 02:54:21 +0000

    modified:   frontend/src/screens/Home.jsx

--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -1,7 +1,7 @@
 import FeaturedItems from '../components/FeaturedItems';
 import ProductListing from '../components/ProductListing';
 import Categories from '../components/categories';
-import { useDispatch, useSelector } from 'react-redux';
+import {  useSelector } from 'react-redux';
 
 import Footer from '../components/Footer';
 
@@ -19,19 +19,6 @@ const [phone, setPhone] = useState('');
 const currency="GHS"
 
 
-const componentProps = {
-  email,
-  amount,
-  currency,
-  metadata: {
-    name,
-    phone,
-  },
-  publicKey,
-  text: 'Pay Now',
-  onSuccess: () => alert('Thanks for doing business with us! Come back soon!!'),
-  onClose: () => alert("Wait! You need this oil, don't go!!!!"),
-};
   const { isOpen } = useSelector((state) => state.chatModal);
 
   return (
@@ -44,49 +31,7 @@ const componentProps = {
       </div>
       <Chat />
 
-      <div className='container'>
-        <div className='item'>
-          <div className='overlay-effect'></div>
-          <img
-            className='item-image'
-            src='https://images.unsplash.com/photo-1526947425960-945c6e72858f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80'
-            alt='product'
-          />
-          <div className='item-details'>
-            <p className='item-details__title'>Coconut Oil</p>
-            <p className='item-details__amount'>{amount / 100}</p>
-          </div>
-        </div>
-        <div className='checkout'>
-          <div className='checkout-form'>
-            <div className='checkout-field'>
-              <label>Name</label>
-              <input
-                type='text'
-                id='name'
-                onChange={(e) => setName(e.target.value)}
-              />
-            </div>
-            <div className='checkout-field'>
-              <label>Email</label>
-              <input
-                type='text'
-                id='email'
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            <div className='checkout-field'>
-              <label>Phone</label>
-              <input
-                type='text'
-                id='phone'
-                onChange={(e) => setPhone(e.target.value)}
-              />
-            </div>
-            <PaystackButton className='paystack-button' {...componentProps} />
-          </div>
-        </div>
-      </div>
+
     </>
   );
 };
